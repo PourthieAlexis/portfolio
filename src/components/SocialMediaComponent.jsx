af96d7e49c503b14a1c5b69e1053df06f6316955
Alexis <alexispourthie@gmail.com>
2023-12-11 15:35:00 +0100

    :construction:

--- src/components/SocialMediaComponent.jsx
+++ src/components/SocialMediaComponent.jsx
@@ -1,4 +1,3 @@
-import linkedIn from "../assets/images/React_logo.png";
 import { MailOutline } from "react-ionicons";
 import { CallOutline } from "react-ionicons";
 import { PersonOutline } from "react-ionicons";
@@ -72,7 +71,6 @@ const SocialMediaComponent = () => {
         rel="noopener noreferrer"
       >
         <p>LinkedIn</p>
-        <img src={linkedIn} alt="Logo linkedIn" width="32" height="32" />
       </LinkedinIcon>
       <MailIcon href="mailto:alexispourthie@gmail.com">
         <p>Mail</p>
